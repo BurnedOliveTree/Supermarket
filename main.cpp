c71f2c3d9470a3679ceabd45c7d0a1fe364a1ebe
Paweł <pawelmuller42@gmail.com>
2020-05-16 13:51:24 +0200

    Added scratch of main argument handling.

--- main.cpp
+++ main.cpp
@@ -13,9 +13,12 @@
 #include "Tests/TestInvoice.hpp"
 
 int main(int argc, char* argv[]) {
-    if (argc > 1) {
-        std::cout << "this function is yet to be handled" << std::endl;
+    switch(argc) {
+        case 1:     break; // brak argumentow - prosba o wpisanie?
+        case 2:     break; // nazwa pliku - odczyt z pliku
+        default:    break; // moze po kolei: liczba cykli symulacji, ilosc klientow, ilosc pracownikow, ilosc kas
     }
+    
     TestInvoice invoiceTest;
     invoiceTest.menu();
     return 0;
