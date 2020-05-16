58c8ffc09cd5ceb50769f22700d4ea9f23b41dcf
Paweł <pawelmuller42@gmail.com>
2020-05-16 12:17:29 +0200

    Removed main.h

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@
 //  Created by Ksawery Chodyniecki and Paweł Müller.
 //
 
-#include "main.h"
+#include "Base.hpp"
+#include "Product.hpp"
+#include "Customer.hpp"
+#include "Employee.hpp"
+#include "CashDesk.hpp"
+#include "Shop.hpp"
+#include "Tests/TestInvoice.hpp"
 
 int main(int argc, char* argv[]) {
     if (argc > 1) {
