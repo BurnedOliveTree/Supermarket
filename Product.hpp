8d62cf5ed416c27de34f36bfdee886851a9f2f13
Paweł <pawelmuller42@gmail.com>
2020-05-31 18:48:53 +0200

    Added Product interface.

--- Product.hpp
+++ Product.hpp
@@ -14,7 +14,30 @@ using namespace std;
 
 enum Measure{pcs, g};
 
-class Product {
+class ProductInterface {
+public:
+    // Getters:
+    virtual string getName() const = 0;
+    virtual unsigned short getID() const = 0;
+    virtual unsigned int getPrice() const = 0;
+    virtual unsigned short getVAT() const = 0;
+    virtual unsigned short getQuantity() const = 0;
+    virtual Measure getMeasureUnits() const = 0;
+    
+    // Setters:
+    virtual void setName(string newName) = 0;
+    virtual void setID(unsigned short newID) = 0;
+    virtual void setPrice(unsigned int newPrice) = 0;
+    virtual void setVAT(unsigned short newVAT) = 0;
+    virtual void setQuantity(unsigned short newQuantity) = 0;
+    virtual void addQuantity(unsigned short argQuantity) = 0;
+    virtual void decQuantity(unsigned short argQuantity) = 0;
+    
+    // Additional features:
+    virtual int calculatePriceBrutto() = 0;
+};
+
+class Product : public ProductInterface {
     string name; // A name of the product.
     unsigned short ID; // An identifier of the product.
     unsigned int price; // Price netto in 1/100 PLN (gr).
